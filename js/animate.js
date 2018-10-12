95eda11f0147ae4f18ee30085ba14338a5fb5604
myusername <avondeta3.14@gmail.com>
2018-10-11 23:14:41 -0400

    added sponsors and nime.js

---
+++ js/animate.js
@@ -0,0 +1,14 @@
+// //var $last_known_scroll_position = 0;
+// //var $ticking = false;
+
+// $(window).scroll(() => {
+//     console.log('hi');
+//     // $last_known_scroll_position = $window.scrollY;
+
+//     // if (!$ticking){
+//     //     if ($last_known_scroll_position>1000) {
+//     //         console.log('hi');
+//     //         $ticking = true;
+//     //     };
+//     // }
+// });
